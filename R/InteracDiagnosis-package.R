8959b4393b2f96c6b049454bdf039dce26154b18
Jianyue Bai <jianyue.bai@mail.utoronto.ca>
2025-12-15 22:42:25 -0500

    docu

--- R/InteracDiagnosis-package.R
+++ R/InteracDiagnosis-package.R
@@ -1,9 +1,13 @@
 #' InteracDiagnosis
 #'
-#' A Shiny tool for visualising how adding or omitting covariates affects parameter estimates, focusing on
-#' changes in the coefficient of interest, along with theoretical and bootstrap standard errors, partial (added-variable) plots,
-#' and model-comparison statistics (AIC, BIC, and likelihood-ratio tests).
+#' A Shiny tool for visualising how adding or omitting covariates affects
+#' parameter estimates, focusing on changes in the coefficient of interest,
+#' along with theoretical and bootstrap standard errors, partial
+#' (added-variable) plots, and model-comparison statistics (AIC, BIC, and
+#' likelihood-ratio tests).
+#'
 #' @docType package
 #' @name InteracDiagnosis
 "_PACKAGE"
+
 
