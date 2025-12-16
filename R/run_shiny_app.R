f93f64fe7c6042498179f6803cf304eb75989953
Jianyue Bai <jianyue.bai@mail.utoronto.ca>
2025-12-15 21:31:19 -0500

    run shiny app

--- R/run_shiny_app.R
+++ R/run_shiny_app.R
@@ -1,6 +1,6 @@
 #' @param ... passed to shiny::runApp()
 #' @export
-run_irls_app <- function(...) {
+run_shiny_app <- function(...) {
   app_dir <- system.file("shiny", package = "InteracDiagnosis")
   if (app_dir == "") stop("Shiny app directory not found. Did you install the package?")
   shiny::runApp(app_dir, ...)
